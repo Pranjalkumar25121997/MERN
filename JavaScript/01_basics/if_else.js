bdf561efba63e38c8f5e460a8a903db2fb7746a2
Pranjal Kumar <163907098+Pranjalkumar25121997@users.noreply.github.com>
2024-03-18 23:46:11 +0000

    container config done

---
+++ JavaScript/01_basics/if_else.js
@@ -0,0 +1,6 @@
+let light = "on"
+if(light === "on") {
+    console.log("Light is on");
+} else{
+    console.log("Light is off");
+}
---
+++ JavaScript/02_basics/Practice_QS.js
@@ -0,0 +1,13 @@
+let color = "green"
+
+if(color === "red"){
+    console.log(`Stop!${color}`);
+}
+
+if(color === "yellow") {
+    console.log(`Slow Down ${color}`);
+}
+
+if(color === "green") {
+    console.log(`Go ${color}`);
+}
