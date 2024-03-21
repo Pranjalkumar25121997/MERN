b0e4fbfe99b5b8d840d5a2d9b4ccc42617a5ad33
Pranjal Kumar <163907098+Pranjalkumar25121997@users.noreply.github.com>
2024-03-21 06:13:00 +0000

    container config done

---
+++ JavaScript/02_basics/practice_qs_3.js
@@ -0,0 +1,21 @@
+let Quarter = 1;
+
+switch(Quarter){
+    case 1:
+        console.log("January", "February", "March");
+        break;
+    case 2:
+        console.log("April", "May", "June");
+        break;
+    case 3:
+        console.log("July", "August", "September");
+        break;
+    case 4:
+        console.log("Octuber", "November", "December");
+        break;
+    default:
+        console.log("Invalid Input!");
+        break;
+}
+
+console.log("3 months completed");
