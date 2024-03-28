ad4602cc490d73edb0b4aa9fa57f3c1247090689
Pranjal Kumar <163907098+Pranjalkumar25121997@users.noreply.github.com>
2024-03-28 00:38:09 +0000

    container config done

---
+++ JavaScript/02_basics/alert.js
@@ -0,0 +1,15 @@
+// alert("Hello in the World if JavaScript");
+// console.log("This is a simple log");
+// console.error("Something is wrong");
+// console.warn("Plz check your code");
+
+// let a = prompt("Enter Your name:- ");
+// console.log(a);
+
+let firstNmae = prompt("Enter your first name:- ");
+let lastNmae = prompt("Enter your last name:- ");
+
+// console.log("Welcome"+ " "+ firstNmae + " "+ lastNmae+"!");
+
+let msg = ("Wlecome "+firstNmae+" "+ lastNmae+"!");
+alert(msg);
