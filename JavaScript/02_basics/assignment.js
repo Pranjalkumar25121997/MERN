befd608b1490450778ab8862ea36445c17f86694
Pranjal Kumar <163907098+Pranjalkumar25121997@users.noreply.github.com>
2024-03-28 07:24:34 +0000

    container config done

---
+++ JavaScript/02_basics/assignment.js
@@ -0,0 +1,39 @@
+// let num = 30;
+// if(num % 10 === 0){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
+
+// let name = prompt("Enter your name:- ");
+// let age = prompt("Enter your age:- ");
+// alert(`name is ${name} ${age} years old`)
+
+// let Quarter = 4;
+
+// switch(Quarter){
+//     case 1: console.log("January,February,March");
+//     break;
+    
+//     case 2: console.log("April,May,June");
+//     break;
+
+//     case 3: console.log("July,August,September");
+//     break;
+
+//     case 4: console.log("October,November,December");
+// }
+
+// let name = "Abhinav"
+
+// if((name[0] === 'A' ||  'a') && (name.length>5)){
+//     console.log("Golden string");
+// }else{
+//     console.log("not a a golden string");
+// }
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+
